ae715066059535bd35c6c31c30065ccce02fc691
Jonatan Medina <jonatan02medina@gmail.com>
2026-05-03 20:01:05 -0600

    - Creando componente reutilizable CustomBotton - Implementando componente reutilizable en App.tsx -Aplicando estilizacion basica con StyleSheet de RN

--- MiPrimeraApp/App.tsx
+++ MiPrimeraApp/App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomButton from './src/components/CustomButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <CustomButton title={"App"} 
+      onPress={()=>{
+        console.log('Press desde boton App')
+        }} 
+      />
+      <CustomButton title={"Secondary Button "} 
+      onPress={()=>{
+        console.log('Press desde boton Secundario')
+        }} 
+      />
     </View>
   );
 }
