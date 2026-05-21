a96d1ba508404c292495c9c3e1a814db616a16ae
Jonatan Medina <jonatan02medina@gmail.com>
2026-05-20 23:02:08 -0600

    -implementacion de AuthContext (tipado de contexto, creacion de provider para inyeccion y exposicion de contexto, creacion de hook para uso de contexto) -inyeccion de contexto en app por medio de provider -uso de contexto en pantallas login y home(utilizando funcion login y el valor de user)

--- MiPrimeraApp/src/components/screens/HomeScreen.tsx
+++ MiPrimeraApp/src/components/screens/HomeScreen.tsx
@@ -3,11 +3,16 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParmList } from "../navigation/StackNavigator";
 import { StatusBar } from "expo-status-bar";
 import CustomButton from "../CustomButton";
+import { useAuth } from "../contexts/AuthContext";
 
 
 type Props = NativeStackScreenProps<RootStackParmList,'Home'>;
 
 export default function HomeScreen({route, navigation}: Props){
+
+    const {user} = useAuth();
+
+
     const {email} = route.params;
     const handleLoadSetting = () => {
             navigation.navigate('UserTabs');
@@ -16,7 +21,7 @@ export default function HomeScreen({route, navigation}: Props){
     return(
         <View>
                 <StatusBar style="auto" />
-            <Text>Hola {email}, Bienvenido a Home</Text>
+            <Text>Hola {user?.email}, Bienvenido a Home</Text>
             <CustomButton title={"Hola"} onPress={handleLoadSetting} />
         </View>
     )
